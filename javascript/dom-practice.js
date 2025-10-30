562e021cac9ca079f7141d0b23333f19f63e2041
Abhijeet Swain <abhijeetswain21@gmail.com>
2025-10-30 09:08:30 +0530

    Delete javascript/dom-practice.js

--- javascript/dom-practice.js
+++
@@ -1,5 +0,0 @@
-// DOM Practice (only runs in browser console)
-document.body.style.backgroundColor = "#f0f0f0";
-const heading = document.createElement("h1");
-heading.textContent = "Learning JavaScript & React 🚀";
-document.body.appendChild(heading);
