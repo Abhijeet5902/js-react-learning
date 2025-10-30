d5cfc8b66c9148021a2e7bd0e63b38981e96eb10
Abhijeet Swain <abhijeetswain21@gmail.com>
2025-10-30 09:06:40 +0530

    Update loops.js

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,4 +1,10 @@
-// Loop practice
+// Loop Practice
 for (let i = 1; i <= 5; i++) {
-  console.log("Number:", i);
+  console.log(`For Loop Count: ${i}`);
+}
+
+let j = 1;
+while (j <= 3) {
+  console.log(`While Loop Count: ${j}`);
+  j++;
 }
